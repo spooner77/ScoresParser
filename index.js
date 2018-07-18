bdc5be671a6259672e7da07a790be6b451e67caa
Andrew Chuhlomin <achuhlomin@klika-tech.com>
2018-07-18 17:55:56 +0300

    Fix id

--- index.js
+++ index.js
@@ -12,19 +12,22 @@ const getFormatted = (s, list) => {
   }
 };
 
+const playerId = 'e8754ee9d97da55ed7f004f7b785f24d';
+
 fetch('https://hola.org/challenges/haggling/scores/standard')
   .then(function(response) {
     return response.json();
   })
   .then(function(data) {
-    const scores = _.map(data, (value, id) => ({ id, ...value.all })).filter(s => s.sessions > 100);
+    const scores = _.map(data, (value, id) => ({ id, ...value.all }))
+      .filter(s => s.sessions > 100 || s.id === playerId);
 
     const list = _.sortBy(scores, s => s.score / s.sessions).map((s, index) => ({
       ...s,
       index,
     }));
 
-    const my = _.find(list, s => s.id === 'e8754ee9d97da55ed7f004f7b785f24d');
+    const my = _.find(list, s => s.id === playerId);
 
     const top = list.slice(Math.max(list.length - 3, 1)).map(s => getFormatted(s, list));
 
