6dfb4e6689030e565c40f3b4d77c19c13a71a364
Eugene Podiachev <epodiachev@klika-tech.com>
2018-07-20 00:49:31 +0300

    fixed bugs

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const getFormatted = (s, list) => {
   }
 };
 
-const playerId = process.argv.slice(3);
+const playerIds = process.argv.slice(3);
 const arena = process.argv[2];
 
 fetch(`https://hola.org/challenges/haggling/scores/${arena}`)
@@ -22,18 +22,18 @@ fetch(`https://hola.org/challenges/haggling/scores/${arena}`)
   })
   .then(function(data) {
     const scores = _.map(data, (value, id) => ({ id, ...value.all }));
-    const player = _.find(scores, s => s.id === playerId);
+    const players = _.find(scores, s => playerIds.includes(s.id));
 
     const offset = 1000;
-    const rating = scores.filter(s => s.sessions >= offset || s.id === playerId);
+    const rating = scores.filter(s => s.sessions >= offset || playerIds.includes(s.id));
 
     const list = _.sortBy(rating, s => s.score / s.sessions).map((s, index) => ({
       ...s,
       index,
-    }));
+  }));
 
+    const my = _.filter(list, s => playerIds.includes(s.id));
     const top = list.slice(Math.max(list.length - 3, 1)).map(s => getFormatted(s, list));
-    const my = _.filter(list, s => playerId.includes(s.id));
     const myL = my.map(s => getFormatted(s, list));
 
     console.log('=========== PLAYERS ===========');
@@ -45,7 +45,7 @@ fetch(`https://hola.org/challenges/haggling/scores/${arena}`)
       console.log(top);
     }
 
-    if (player) {
+    if (players) {
       console.log('=========== MY ===========');
       console.log(myL);
     }
